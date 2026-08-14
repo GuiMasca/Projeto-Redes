c8033a6fd52991776042f9819e7e8af90e886ec9
brunoselingardi <brunoselingardi@gmail.com>
2026-08-14 10:42:54 -0300

    Esqueleto da conexão e delete dos testes

---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+from datetime import datetime
+
+
+datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+HOST = ''
+PORT = 50007
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen(1)
+
+    print("Servidor aguardando conexão...")
+
+    conn, addr = s.accept()
+
+    with conn:
+        print("Conexão estabelecida com:", addr)
+        print("/n/n")
+
+        horario = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        msg1 = f"{horario} - CONECTADO!! /n/n"
+
+        conn.sendall(msg1.encode())
+
+        print("Mensagem enviada", msg1)
