4093c1fb6437244099274250c0f5c4eeb0a34a02
brunoselingardi <brunoselingardi@gmail.com>
2026-08-23 22:50:57 -0300

    thread 1 criada

--- servernew.py
+++ servernew.py
@@ -1,8 +1,27 @@
 import socket
+import threading
 from datetime import datetime
 
-HOST = ''   #host aberto para aceitar conexões de qualquer endereço (mais flexivel) 
-PORT = 50007              
+
+HOST = ''   #host aberto para aceitar conexões de qualquer endereço (mais flexivel)
+PORT = 50007
+
+
+def atender_cliente(conn, addr):
+    horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")      #define o texto do horario: dia, mes, ano, hora, minuto, segundo
+    with conn:
+        print('conexão estabelecida com', addr, 'às', horario)
+        msg = f'{horario} - CONECTADO!!\n' #mensagem com o horario de conexão do cliente
+        conn.sendall(msg.encode())  #envia a mensagem em bytes para o cliente
+
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
+    horario_fim = datetime.now().strftime("%d/%m/%Y %H:%M:%S")  #horario_fim deve ser diferente
+    print('Conexão encerrada por', addr, 'às', horario_fim)
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     #define opção do socket(no nivel do socket, qual opção, ligado)
@@ -10,17 +29,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(5)     #de padrão vem 1, mas eu quero que mais clientes possam se conectar ao mesmo tempo, então coloco 5.
 
-    while True:     #permite que o servidor "viva para sempre"
-        conn, addr = s.accept()  #aceita a conexão do cliente, agora dentro do loop
-        horario = datetime.now().strftime("%d/%m/%Y %H:%M:%S")      #define o texto do horario: dia, mes, ano, hora, minuto, segundo
-        with conn:
-            print('conexão estabelecida com', addr, 'às', horario)
-            msg = f'{horario} - CONECTADO!!\n' #mensagem com o horario de conexão do cliente
-            conn.sendall(msg.encode())  #envia a mensagem em bytes para o cliente
-
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                conn.sendall(data)
-        horario_fim = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        print('Conexão encerrada por', addr, 'às', horario_fim)
+    while True:
+        conn, addr = s.accept()
+        t1 = threading.Thread(target=atender_cliente, args=(conn, addr))  #cria uma thread ("funcionario") para cada cliente que se conecta
+        t1.start() #coloca a thread ("funcionario") para trabalhar
+        t1.join() #proximo accept() só depois que o cliente atual saír
